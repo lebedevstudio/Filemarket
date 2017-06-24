a6da96acfc7dc002b92785802f343ca3c4a52860
lebedevstudio <bryantsrs@yandex.ru>
2017-06-24 12:21:26 +0700

    Some indentation fixes again

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,14 @@ Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 
 Route::group(['prefix' => 'account', 'middleware' => ['auth'], 'namespace' => 'Account'], function () {
-	Route::get('/', 'AccountController@index')->name('account');
+    Route::get('/', 'AccountController@index')->name('account');
 
-	Route::group(['prefix' => 'files'], function () {
-		Route::get('/', 'FileController@index')->name('account.files.index');
+    Route::group(['prefix' => 'files'], function () {
+        Route::get('/', 'FileController@index')->name('account.files.index');
         Route::get('/{file}/edit', 'FileController@edit')->name('account.files.edit');
         Route::patch('/{file}', 'FileController@update')->name('account.files.update');
-		Route::post('/{file}', 'FileController@store')->name('account.files.store');
-		Route::get('/create', 'FileController@create')->name('account.files.create.start');
-		Route::get('/{file}/create', 'FileController@create')->name('account.files.create');
-	});
+        Route::post('/{file}', 'FileController@store')->name('account.files.store');
+        Route::get('/create', 'FileController@create')->name('account.files.create.start');
+        Route::get('/{file}/create', 'FileController@create')->name('account.files.create');
+    });
 });
